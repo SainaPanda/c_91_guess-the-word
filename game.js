15109ecb1db064bc4395bfb488d08ffdcd08c8b8
SainaPanda <88354134+SainaPanda@users.noreply.github.com>
2022-04-30 12:38:10 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 player1_name = localStorage.getItem("player1_name");
 player2_name = localStorage.getItem("player2_name");
 console.log(player1_name);
+console.log(player2_name);
 player1_score=0;
 player2_score=0;
 
